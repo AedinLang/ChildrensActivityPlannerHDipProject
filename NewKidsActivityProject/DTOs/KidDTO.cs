d20af05e602e1c17220fdc98a4e5300744fa28c9
Clare Shiels <c_cashin@hotmail.com>
2016-04-27 20:40:30 +0100

    trying to update, going to commit these untracked files

---
+++ NewKidsActivityProject/DTOs/KidDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NewKidsActivityProject.DTOs
+{
+    public class KidDTO
+    {
+        public string FirstName { get; set; }
+        public string LastName { get;set;}
+        public DateTime DOB { get; set; }
+        public string Enrolment { get; set; }
+    }
+}
